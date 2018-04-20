02cc87526a35c2bfe230f4cff9c291ab0d99ff67
wangxum <1736096063@qq.com>
2018-04-20 16:02:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <p>
+      <button @click="increment">+</button>
+    </p>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store ({
+  state: {
+    count: 0
+  },
+  mutations:{
+    increment ( state ){
+      state.count++;
+    }
+  }
+});
+
+import counter from "./counter.vue"
+
+export default {
+  name: 'app',
+  store,
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    counter
+  },
+  computed: {
+    count () {
+	    return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    {{count}}
+    <child/>
+    </div>
+</template>
+<script>
+    import child from './child.vue'
+    export default{
+        computed:{
+            count(){
+                return this.$store.state.count;
+            }
+        },
+        components:{
+            child
+        }
+    }    
+</script>
